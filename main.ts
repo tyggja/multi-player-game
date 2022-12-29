c68828796045df159667b46b9090fa11c1099432
tyggja <116696488+tyggja@users.noreply.github.com>
2022-12-29 00:18:49 -0500

    Starting from 0 (center point) towards the outskirts

--- main.ts
+++ main.ts
@@ -7,20 +7,35 @@
     // plot(x,y) --> turn on LED on x,y position
     // unplot(x,y) --> turn off LED on x,y position
     // 1024 % 5 = 204.8 (205)
-let filter = 170; // default 205 from
-let last_x = 0;
-let last_y = 0;
-
+//let filter = 170; // default 205 from
+//let last_x = 0;
+//let last_y = 0;
+let center_sprite = game.createSprite(2, 2);
 basic.forever(function () {
-    last_x = input.acceleration(Dimension.X) / filter
-    last_y = input.acceleration(Dimension.Y) / filter
-    led.plot(last_x, last_y)
-    pause(200)
-    if((last_x != input.acceleration(Dimension.X) / filter) && 
-      (last_y != input.acceleration(Dimension.Y) / filter)) {
-          led.unplot(last_x, last_y)
-      }
-    
+    //last_x = input.acceleration(Dimension.X) / filter
+    //last_y = input.acceleration(Dimension.Y) / filter
+    //led.plot(last_x, last_y)
+    //pause(200)
+    //if((last_x != input.acceleration(Dimension.X) / filter) && 
+    //  (last_y != input.acceleration(Dimension.Y) / filter)) {
+    //      led.unplot(last_x, last_y)
+    //  }
+    if(input.acceleration(Dimension.X) < 0) {
+        center_sprite.change(LedSpriteProperty.X, -1)
+        pause(200)
+    }
+    if(input.acceleration(Dimension.X) > 0) {
+        center_sprite.change(LedSpriteProperty.X, 1)
+        pause(200)
+    }
+    if(input.acceleration(Dimension.Y) < 0) {
+        center_sprite.change(LedSpriteProperty.Y, -1)
+        pause(200)
+    }
+    if(input.acceleration(Dimension.Y) > 0) {
+        center_sprite.change(LedSpriteProperty.Y, 1)
+        pause(200)
+    }
     
 
     //music.playTone(440, 50)
@@ -30,5 +45,7 @@ basic.forever(function () {
 })
 
 input.onLogoEvent(TouchButtonEvent.Touched, () => {
-    basic.clearScreen();
+    //basic.clearScreen();
+    game.createSprite(2, 2)
+
 })
