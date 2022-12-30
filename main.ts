3d0aee5fd83639a18ff27c604104b8cbabb17155
tyggja <116696488+tyggja@users.noreply.github.com>
2022-12-30 11:31:54 -0500

    use plot() instead of sprite --> not great

--- main.ts
+++ main.ts
@@ -1,54 +1,27 @@
+let filter = 200; // default 205 from
 
-//1). Create a menu 
-    // game.createSprite(0, 2)
-    // game.createSprite(1, 2)
+let start_x = 2 // |
+let start_y = 2 // |----> to start in the middle of the board (center-point)
+
+input.onButtonPressed(Button.AB, () => {
+        
+    basic.clearScreen()
+    pause(500)
+
+    basic.forever(function () {
     
-//2). Move pixel by tilting the board
-    // plot(x,y) --> turn on LED on x,y position
-    // unplot(x,y) --> turn off LED on x,y position
-    // 1024 % 5 = 204.8 (205)
-//let filter = 170; // default 205 from
-//let last_x = 0;
-//let last_y = 0;
-let center_sprite = game.createSprite(2, 2);
-basic.forever(function () {
-    //last_x = input.acceleration(Dimension.X) / filter
-    //last_y = input.acceleration(Dimension.Y) / filter
-    //led.plot(last_x, last_y)
-    //pause(200)
-    //if((last_x != input.acceleration(Dimension.X) / filter) && 
-    //  (last_y != input.acceleration(Dimension.Y) / filter)) {
-    //      led.unplot(last_x, last_y)
-    //  }
-    if(input.acceleration(Dimension.X) < 0) {
-        center_sprite.change(LedSpriteProperty.X, -1)
-        pause(200)
-    }
-    if(input.acceleration(Dimension.X) > 0) {
-        center_sprite.change(LedSpriteProperty.X, 1)
-        pause(200)
-    }
-    if(input.acceleration(Dimension.Y) < 0) {
-        center_sprite.change(LedSpriteProperty.Y, -1)
-        pause(200)
-    }
-    if(input.acceleration(Dimension.Y) > 0) {
-        center_sprite.change(LedSpriteProperty.Y, 1)
-        pause(200)
-    }
-    
+        // start in the center of the LED array (x = 2)
+        if (input.acceleration(Dimension.X) >= 0) {
+            led.plot(start_x + Math.ceil(input.acceleration(Dimension.X) / filter), 2)
+        }
+        if (input.acceleration(Dimension.X) <= 0) {
+            led.plot(start_x + Math.ceil(input.acceleration(Dimension.X) / filter), 2)
+        }
 
-    //music.playTone(440, 50)
-    //led.unplot(input.acceleration(Dimension.X) / filter,
-            //input.acceleration(Dimension.Y) / filter)
-
-})
-
-input.onLogoEvent(TouchButtonEvent.Touched, () => {
-    basic.clearScreen();
-    //game.createSprite(2, 2)
-
-})
-input.onButtonPressed(Button.B, () => {
-    //game.createSprite(center_sprite.x, Dimension.Y)
+        pause(100)
+        serial.writeLine("x-raw: " + input.acceleration(Dimension.X))
+        serial.writeLine("x-proc: " + (start_x + Math.ceil(
+            input.acceleration(Dimension.X)) / filter))
+        //serial.writeLine("y: " + input.acceleration(Dimension.Y) / filter)
+    })  
 })
