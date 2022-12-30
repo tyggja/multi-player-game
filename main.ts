799cb8540433d265b4d57f81fe37574054994475
tyggja <116696488+tyggja@users.noreply.github.com>
2022-12-30 16:49:29 -0500

    using buttons instead of tilt to navigate across the LED array

--- main.ts
+++ main.ts
@@ -1,27 +1,52 @@
-let filter = 200; // default 205 from
 
-let start_x = 2 // |
-let start_y = 2 // |----> to start in the middle of the board (center-point)
+// Track moving x,y coordinates
+let current_x = 2;
+let current_y = 2;
+
+// Create a sprite at position x:2, y:2 (center)
+let moving_led = game.createSprite(current_x, current_y)
+moving_led.off()
 
 input.onButtonPressed(Button.AB, () => {
-        
     basic.clearScreen()
     pause(500)
+    moving_led.on()
+    pause(500)
 
-    basic.forever(function () {
-    
-        // start in the center of the LED array (x = 2)
-        if (input.acceleration(Dimension.X) >= 0) {
-            led.plot(start_x + Math.ceil(input.acceleration(Dimension.X) / filter), 2)
-        }
-        if (input.acceleration(Dimension.X) <= 0) {
-            led.plot(start_x + Math.ceil(input.acceleration(Dimension.X) / filter), 2)
-        }
+    // The (B) button controlls the X axis
+    input.onButtonPressed(Button.B, () => {
+        if (current_x >= 4) {
+            
+            // Reset the X counter
+            current_x = 0;
 
-        pause(100)
-        serial.writeLine("x-raw: " + input.acceleration(Dimension.X))
-        serial.writeLine("x-proc: " + (start_x + Math.ceil(
-            input.acceleration(Dimension.X)) / filter))
-        //serial.writeLine("y: " + input.acceleration(Dimension.Y) / filter)
-    })  
-})
+            moving_led.goTo(current_x, current_y)
+            
+        } else {
+            moving_led.changeXBy(1);
+            // Increase X counter
+            current_x++;
+        }
+        serial.writeLine(JSON.stringify("X: " + current_x))
+        
+    })
+
+    // The (A) button controlls the Y axis
+    input.onButtonPressed(Button.A, () => {
+        if (current_y >= 4) {
+            
+            // Reset the Y counter
+            current_y = 0;
+            
+            moving_led.goTo(current_x, current_y)
+            
+        } else {
+            moving_led.changeYBy(1);
+            
+            // Increase Y counter
+            current_y++;
+        }
+        serial.writeLine(JSON.stringify("Y: " + current_y))
+
+    })
+});
