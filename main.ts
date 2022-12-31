ad622819df0905079d3b4d488499aec5a795a32e
tyggja <116696488+tyggja@users.noreply.github.com>
2022-12-31 00:37:32 -0500

    working on long button press

--- main.ts
+++ main.ts
@@ -1,19 +1,6 @@
-
-// Track moving x,y coordinates
-let current_x = 2;
-let current_y = 2;
-
-
-
-input.onButtonPressed(Button.AB, () => {
-
-    // Create a sprite at position x:2, y:2 (center)
-    let moving_led = game.createSprite(current_x, current_y)
-    moving_led.off()
-
-    basic.clearScreen()
-    music.playSoundEffect(
-        music.builtinSoundEffect(soundExpression.spring), SoundExpressionPlayMode.InBackground)
+input.onButtonPressed(Button.AB, function () {
+    moving_led.delete()
+    moving_led = game.createSprite(2, 2)
     basic.showAnimation(`
         . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 . .#. . . .#. . . .#. . . .#. . . .#. . . .#. .
@@ -21,15 +8,11 @@ input.onButtonPressed(Button.AB, () => {
 . .#. . . .#. . . .#. . . .#. . . .#. . . .#. .
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 `, 500)
-    
-    pause(500)
-    moving_led.on()
-    moving_led.setBlink(300)
-    pause(500)
-
-    // The (B) button controlls the X axis
-    input.onButtonPressed(Button.B, () => {
-        music.playTone(Note.C5, 50)
+pause(500)
+moving_led.on()
+moving_led.setBlink(600)
+input.onButtonPressed(Button.B, () => {
+        //music.playTone(Note.C5, 50)
         if (current_x >= 4) {
             
             // Reset the X counter
@@ -45,10 +28,8 @@ input.onButtonPressed(Button.AB, () => {
         serial.writeLine(JSON.stringify("X: " + current_x))
         
     })
-
-    // The (A) button controlls the Y axis
-    input.onButtonPressed(Button.A, () => {
-        music.playTone(Note.B5, 50)
+input.onButtonPressed(Button.A, () => {
+        //music.playTone(Note.B4, 50)
         if (current_y >= 4) {
             
             // Reset the Y counter
@@ -63,6 +44,32 @@ input.onButtonPressed(Button.AB, () => {
             current_y++;
         }
         serial.writeLine(JSON.stringify("Y: " + current_y))
-
     })
-});
+basic.forever(() => {
+        // // The (A) button or the (B) button on long press, selects the LED 
+        // if((input.buttonIsPressed(Button.B) || (input.buttonIsPressed(Button.A)))) {
+        //     //TODO: if the selected LED is already on, toggle it off
+        //     //TODO: add sound for either buttons
+        //     // Create a new Sprite and check if it's touching the old one
+        //     let new_led = game.createSprite(current_x, current_y)
+
+        //     if (new_led.isTouching(moving_led)) {
+        //         new_led.delete()
+        //         moving_led.delete()
+        //     }
+
+        // }
+
+
+        input.onButtonPressed(Button.A, function() {
+            TouchButtonEvent.LongPressed
+        })
+    })
+})
+let moving_led: game.LedSprite = null
+// Track moving x,y coordinates
+let current_x = 2
+let current_y = 2
+// Create a sprite at position x:2, y:2 (center)
+moving_led = game.createSprite(current_x, current_y)
+moving_led.off()
