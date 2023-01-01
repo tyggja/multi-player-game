b5dd6dc8a56c1fbd6ac3819d80d62908a9c92b87
tyggja <116696488+tyggja@users.noreply.github.com>
2023-01-01 01:41:14 -0500

    handle long and short press for both Button(A) and Button(B)

--- main.ts
+++ main.ts
@@ -1,75 +1,121 @@
+let moving_led: game.LedSprite = null;
+// Track moving x,y coordinates
+let current_x = 2;
+let current_y = 2;
+// Create a sprite at position x:2, y:2 (center)
+moving_led = game.createSprite(current_x, current_y);
+moving_led.off();
+
 input.onButtonPressed(Button.AB, function () {
-    moving_led.delete()
-    moving_led = game.createSprite(2, 2)
-    basic.showAnimation(`
+    moving_led.delete();
+    moving_led = game.createSprite(2, 2);
+    basic.showAnimation(
+                `
+            . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+        . .#. . . .#. . . .#. . . .#. . . .#. . . .#. .
+        .#.#. .# # #. .#.#. .# # #. .#.#. .# # #.
+        . .#. . . .#. . . .#. . . .#. . . .#. . . .#. .
         . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-. .#. . . .#. . . .#. . . .#. . . .#. . . .#. .
-.#.#. .# # #. .#.#. .# # #. .#.#. .# # #.
-. .#. . . .#. . . .#. . . .#. . . .#. . . .#. .
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-`, 500)
-pause(500)
-moving_led.on()
-moving_led.setBlink(600)
-input.onButtonPressed(Button.B, () => {
+        `,
+            500
+    );
+    
+    pause(500);
+    moving_led.on();
+    moving_led.setBlink(600);
+    
+    input.onButtonPressed(Button.B, () => {
         //music.playTone(Note.C5, 50)
         if (current_x >= 4) {
-            
             // Reset the X counter
             current_x = 0;
 
-            moving_led.goTo(current_x, current_y)
-            
+            moving_led.goTo(current_x, current_y);
         } else {
             moving_led.changeXBy(1);
             // Increase X counter
             current_x++;
         }
-        serial.writeLine(JSON.stringify("X: " + current_x))
-        
-    })
-input.onButtonPressed(Button.A, () => {
-        //music.playTone(Note.B4, 50)
-        if (current_y >= 4) {
-            
-            // Reset the Y counter
-            current_y = 0;
-            
-            moving_led.goTo(current_x, current_y)
-            
+        serial.writeLine(JSON.stringify("X: " + current_x));
+    });
+
+    let longPressDuration = 1000; // 1 second
+    let buttonPressStartTime = 0;
+    let buttonPressDuration = 0;
+
+    basic.forever(() => {
+
+        // Button (A)
+        if (input.buttonIsPressed(Button.A)) {
+            // Button A is being held down
+            if (buttonPressStartTime == 0) {
+                // This is the first iteration of the loop where the button is being held down
+                buttonPressStartTime = input.runningTime();
+            }
+            buttonPressDuration = input.runningTime() - buttonPressStartTime;
+            if (buttonPressDuration >= longPressDuration) {
+                // Button A has been held down for more than 1 second
+                // TODO: Handle long press
+                
+                buttonPressStartTime = 0;
+                buttonPressDuration = 0;
+            }
         } else {
-            moving_led.changeYBy(1);
-            
-            // Increase Y counter
-            current_y++;
+            // Button A is not being held down
+            if (buttonPressDuration > 0 && buttonPressDuration < longPressDuration) {
+                // Button A was released before 1 second
+                // Handle short press
+                //music.playTone(Note.B4, 50)
+                if (current_y >= 4) {
+                    // Reset the Y counter
+                    current_y = 0;
+                    moving_led.goTo(current_x, current_y);
+                } else {
+                    moving_led.changeYBy(1);
+                    
+                    // Increase Y counter
+                    current_y++;
+                }
+            }
+            buttonPressStartTime = 0;
+            buttonPressDuration = 0;
         }
-        serial.writeLine(JSON.stringify("Y: " + current_y))
-    })
-basic.forever(() => {
-        // // The (A) button or the (B) button on long press, selects the LED 
-        // if((input.buttonIsPressed(Button.B) || (input.buttonIsPressed(Button.A)))) {
-        //     //TODO: if the selected LED is already on, toggle it off
-        //     //TODO: add sound for either buttons
-        //     // Create a new Sprite and check if it's touching the old one
-        //     let new_led = game.createSprite(current_x, current_y)
 
-        //     if (new_led.isTouching(moving_led)) {
-        //         new_led.delete()
-        //         moving_led.delete()
-        //     }
+        // Button (B)
+        if (input.buttonIsPressed(Button.B)) {
+            // Button B is being held down
+            if (buttonPressStartTime == 0) {
+                // This is the first iteration of the loop where the button is being held down
+                buttonPressStartTime = input.runningTime();
+            }
+            buttonPressDuration = input.runningTime() - buttonPressStartTime;
+            if (buttonPressDuration >= longPressDuration) {
+                // Button B has been held down for more than 1 second
+                // TODO: Handle long press
+                
+                buttonPressStartTime = 0;
+                buttonPressDuration = 0;
+            }
+        } else {
+            // Button B is not being held down
+            if (buttonPressDuration > 0 && buttonPressDuration < longPressDuration) {
+                // Button B was released before 1 second
+                // Handle short press
+                //music.playTone(Note.C5, 50)
+                if (current_x >= 4) {
+                    // Reset the X counter
+                    current_x = 0;
 
-        // }
-
-
-        input.onButtonPressed(Button.A, function() {
-            TouchButtonEvent.LongPressed
-        })
-    })
-})
-let moving_led: game.LedSprite = null
-// Track moving x,y coordinates
-let current_x = 2
-let current_y = 2
-// Create a sprite at position x:2, y:2 (center)
-moving_led = game.createSprite(current_x, current_y)
-moving_led.off()
+                    moving_led.goTo(current_x, current_y);
+                } else {
+                    moving_led.changeXBy(1);
+                    // Increase X counter
+                    current_x++;
+                }
+            }
+            
+            buttonPressStartTime = 0;
+            buttonPressDuration = 0;
+        }
+    });
+});
