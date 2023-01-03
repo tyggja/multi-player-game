0543546cf100b61935b222efbc3657b5f1977594
tyggja <116696488+tyggja@users.noreply.github.com>
2023-01-03 18:55:25 -0500

    fucking-hell-lost-a-bunch-of-work

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 // Track moving x,y coordinates
-let current_x = 2;
-let current_y = 2;
+let current_x = 0
+let current_y = 0
 
 // Set joystick pins &  variables
 pins.analogReadPin(AnalogPin.P0)
@@ -8,12 +8,24 @@ pins.analogReadPin(AnalogPin.P1)
 pins.digitalReadPin(DigitalPin.P2)
 pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 
+let moving_led: game.LedSprite = null;
+
 let val_x, val_y, sw = 0;
 
 input.onButtonPressed(Button.AB, () => {
 
+
+    // Set initial location for X and Y
+    current_x = 2
+    current_y = 2
+    // Clean the moving_led sprite in-between cycles if it already exists
+    if (moving_led) {
+        moving_led.delete()
+    }
+    
+
     // Create a sprite at position x:2, y:2 (center)
-    let moving_led = game.createSprite(current_x, current_y)
+    moving_led = game.createSprite(current_x, current_y)
     moving_led.off()
 
     basic.clearScreen()
@@ -27,55 +39,14 @@ input.onButtonPressed(Button.AB, () => {
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 `, 500)
 
-    pause(500)
+    pause(200)
     moving_led.on()
     moving_led.setBlink(300)
-    pause(500)
-
-    // The (B) button controlls the X axis
-    input.onButtonPressed(Button.B, () => {
-        music.playTone(Note.C5, 50)
-        if (current_x >= 4) {
-
-            // Reset the X counter
-            current_x = 0;
-
-            moving_led.goTo(current_x, current_y)
-
-        } else {
-            moving_led.changeXBy(1);
-            // Increase X counter
-            current_x++;
-        }
-        serial.writeLine(JSON.stringify("X: " + current_x))
-
-    })
-
-    // The (A) button controlls the Y axis
-    input.onButtonPressed(Button.A, () => {
-        music.playTone(Note.B5, 50)
-        if (current_y >= 4) {
-
-            // Reset the Y counter
-            current_y = 0;
-
-            moving_led.goTo(current_x, current_y)
-
-        } else {
-            moving_led.changeYBy(1);
-
-            // Increase Y counter
-            current_y++;
-        }
-        serial.writeLine(JSON.stringify("Y: " + current_y))
-
-    })
+    pause(200)
 
     basic.forever(() => {
+        
         val_x = pins.analogReadPin(AnalogPin.P0)
         val_y = pins.analogReadPin(AnalogPin.P1)
-
-        pause(20)
-        serial.writeLine(`x: ${val_x} y: ${val_y}`)
-    })
-});
+    }
+}
